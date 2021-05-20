964e960ec6e9cfa22277e9f1e74d5e6883193af8
Tharindu Sathischandra <tharindu.sathis@gmail.com>
2021-05-20 14:13:28 +0530

    CODELAB:STEPS:7:The LiveData class

--- app/src/main/java/com/github/tharindusathis/roomwordsample/WordDao.java
+++ app/src/main/java/com/github/tharindusathis/roomwordsample/WordDao.java
@@ -1,5 +1,6 @@
 package com.github.tharindusathis.roomwordsample;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
@@ -19,6 +20,6 @@ public interface WordDao {
     void deleteAll();
 
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    List<Word> getAlphabetizedWords();
+    LiveData<List<Word>> getAlphabetizedWords();
 
 }
