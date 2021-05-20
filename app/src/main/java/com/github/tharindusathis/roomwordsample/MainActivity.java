8f4e1bcd73670c3251f2643ca608e5ecd8f7dd4a
Tharindu Sathischandra <tharindu.sathis@gmail.com>
2021-05-20 15:28:14 +0530

    CODELAB:STEPS:12:Add a RecyclerView

--- app/src/main/java/com/github/tharindusathis/roomwordsample/MainActivity.java
+++ app/src/main/java/com/github/tharindusathis/roomwordsample/MainActivity.java
@@ -1,6 +1,8 @@
 package com.github.tharindusathis.roomwordsample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final WordListAdapter adapter = new WordListAdapter(new WordListAdapter.WordDiff());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/github/tharindusathis/roomwordsample/WordViewHolder.java
@@ -0,0 +1,30 @@
+package com.github.tharindusathis.roomwordsample;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class WordViewHolder extends RecyclerView.ViewHolder {
+
+    private final TextView wordItemView;
+
+    private WordViewHolder(View itemView) {
+        super(itemView);
+        wordItemView = itemView.findViewById(R.id.textView);
+    }
+
+    public void bind(String text) {
+        wordItemView.setText(text);
+    }
+
+    static WordViewHolder create(ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recyclerview_item, parent, false);
+        return new WordViewHolder(view);
+    }
+
+}
