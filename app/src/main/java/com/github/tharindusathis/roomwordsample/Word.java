2e722700b41150438640c2f043c8478ea51d30d0
Tharindu Sathischandra <tharindu.sathis@gmail.com>
2021-05-20 14:07:14 +0530

    CODELAB:STEPS:5:Create an entity

---
+++ app/src/main/java/com/github/tharindusathis/roomwordsample/Word.java
@@ -0,0 +1,23 @@
+package com.github.tharindusathis.roomwordsample;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(@NonNull String mWord) {
+        this.mWord = mWord;
+    }
+
+    public String getmWord() {
+        return mWord;
+    }
+}
