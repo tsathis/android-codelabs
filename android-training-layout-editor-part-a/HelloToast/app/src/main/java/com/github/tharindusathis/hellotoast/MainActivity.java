d264bb5eff1c9349951c89b2a623cefccfcefd52
Tharindu Sathischandra <tharindu.sathis@gmail.com>
2021-05-22 11:39:47 +0530

    android-training-layout-editor-part-a:STEPS:4-end

--- android-training-layout-editor-part-a/HelloToast/app/src/main/java/com/github/tharindusathis/hellotoast/MainActivity.java
+++ android-training-layout-editor-part-a/HelloToast/app/src/main/java/com/github/tharindusathis/hellotoast/MainActivity.java
@@ -3,12 +3,31 @@ package com.github.tharindusathis.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(this, R.string.toast_text, Toast.LENGTH_SHORT).show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(String.format(Locale.ENGLISH, "%d", mCount));
     }
 }
